8c40704a8e919f936c58ab5ef00c18efd4caf195
Maman Sani Ibrahim <40499403+Ibrahim227@users.noreply.github.com>
2023-12-14 21:28:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+alert("This is an external test file!!!");
+
+
+let admin;
+let name;
+
+name = '"Jhon"';
+admin = name;
+alert(admin);
+
+
+let PlanetEarth;
+let CurrentUser;
+
+// let BinaryTest, OctalTest;
+// BinaryTest =  0b00001100101000101001100;
+// OctalTest = 0O1222233347;
+// alert(berea);
+// alert(OctalTest)
+// // console.log(berea);
+
+// let TestBigIntNotationHexa;
+
+// TestBigIntNotationHexa = 0x123243525ABCDEFn;
+// alert(TestBigIntNotationHexa);
+
+// let TestBigIntNotationLiteral;
+
+// TestBigIntNotationLiteral = 1234456789n;
+// alert(TestBigIntNotationLiteral);
+
+let NumericSeparator;
+
+NumericSeparator = 0b0_1000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000n;
+
+alert(NumericSeparator);
+
+console.log('Hello, world!\f');
+
+let TestCal; // declare variable named TestCal
+
+TestCal = 5e2;
+alert(TestCal); // print the result of 5 exponent 2
+alert(100e3); // print the result of 100 exponent  3
+
+alert(0x1234567ABDEFn); // print the result of  hexadecimal number as Bigint
+alert("\xA9");
+
